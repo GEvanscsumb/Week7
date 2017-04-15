048a38479ead2ed779768637d17ff7931c610c95
Trammel May <tramstheman@gmail.com>
2017-04-15 11:15:35 -0700

    Update lab16.py

--- lab16.py
+++ lab16.py
@@ -1,15 +1,45 @@
 """ 
-Trammel May, Gene Evans, Trent Duhart
+Lab16
+@Author Trammel May, Gene Evans, Trent Duhart
 """
 
 """ Make a website showing the top 10 rated movies on IMDB """ 
 
 def makePage():
-  response = getRaw("http://www.imdb.com/chart/top")
-  print response
-
+  setWorkingDir()
+  writeToFile(extractTopTen(getRaw("http://www.imdb.com/chart/top")))
+  
 def getRaw(url):
+  import urllib2
   req = urllib2.Request(url)
   return urllib2.urlopen(req).read()
 
+def makeHTML(topten):
+  html = """<!DOCTYPE html>
+  <html>
+  <body>
+  <ol>"""
+  for movie in topten:
+    html += """<li>""" + movie + """</li>"""
+  html += """</ol>
+  </body>
+  </html>"""
+  return html
+
+def writeToFile(topten):
+  file = open(getMediaPath() + "/top_ten.html", 'w+')
+  file.write(makeHTML(topten))
+  file.close()
+  
+def setWorkingDir():
+  showInformation("Please select the working directory of the project")
+  setMediaPath()
+  
+def extractTopTen(data):
+  import re
+  matcher = "<td class=\"titleColumn\">\n.*\n.*\ntitle=.+\" >(.*)<"
+  return re.findall(matcher, data)[0:10]
+  
+
+  
 
